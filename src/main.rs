25dd8b8c1d41f91704c2a321381dd115fa96ffff
Louis Stockdale-Lees <lslees1346@gmail.com>
2025-10-20 13:57:57 +0100

    output/preproc.i

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,59 @@
 use std::env;
 use std::process::Command;
 use std::process::exit;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 
 fn main()
 {
-    let args: Vec<String> = env::args().collect();
-    
-    if args.len() != 3
-    {
-        eprintln!("INVALID: <source.c>");
-        exit(1);
-    }
+	let args: Vec<String> = env::args().collect();
 
-    let source_file = &args[1];
-    let output_file = "PREPROCESSED_FILE";
+	if args.len() != 2
+	{
+		eprintln!("invalid args");
+		exit(1);
+	}
 
-    // run gcc pre proc
-    let status = Command::new("gcc")
-        .args(&["-E", "-P", source_file, "-o", output_file])
-        .status()
-        .expect("Failed to run gcc");
+	let source = &args[1];
+	let output: PathBuf = Path::new("output").join("preproc.i");
 
-    if let Some(code) = status.code()
-    {
-        exit(code);
-    }
-    else
-    {
-        eprintln!("Process terminated by signal");
-        exit(1);
-    }
+	if !Path::new(source).exists()
+	{
+		eprintln!("invalid source");
+		exit(1);
+	}
+
+	if let Some(p) = output.parent()
+	{
+		if let Err(e) = fs::create_dir_all(p)
+		{
+			eprintln!("mkdir fail - {}", e);
+			exit(1);
+		}
+	}
+	else
+	{
+		exit(1);
+	}
+
+	// run pre proc
+	let status = Command::new("gcc")
+		.arg("-E")
+		.arg("-P")
+		.arg(source)
+		.arg("-o")
+		.arg(&output)
+		.status()
+		.expect("gcc error");
+
+	if let Some(code) = status.code()
+	{
+		exit(code);
+	}
+	else
+	{
+		eprintln!("process terminated");
+		exit(1);
+	}
 }
